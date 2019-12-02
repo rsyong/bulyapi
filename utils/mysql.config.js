042c6d5eeae05546452786ee8f2761ea6f58bf6f
rsyong <409431858!qq.com>
2019-12-02 15:11:04 +0800

    19.12.2.2

--- utils/mysql.config.js
+++ utils/mysql.config.js
@@ -3,7 +3,7 @@ const option = {
     host: "cdb-gj0lltxm.bj.tencentcdb.com",
     user: 'root',
     password: 'rsyong950918',
-    port: '3306',
+    port: '10253',
     database: 'buly'
 }
 exports.init = () => {
